af8b48e6cf483064e1fd0451c7f06394c6a6c883
nicholasfannin-eng <nicholas.fannin@stu.ocsb.ca>
2026-09-16 09:54:42 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -43,8 +43,34 @@ public class Main {
 		System.out.println("Give me another plural noun: ");
 		String pn2 = in.nextLine();
 		System.out.println("Today, "+bp1+"-held "+pn2+" are all the rage.");
+		System.out.println("Give me another another plural noun: ");
+		String pn3 = in.nextLine();
+		System.out.println("Give me another adverb: ");
+		String avrb2 = in.nextLine();
+		System.out.println("Give me another adjective: ");
+		String adj2 = in.nextLine();
+		System.out.println("Give me another noun: ");
+		String n2 = in.nextLine();
+		System.out.println("In restaurants, you find many "+pn3+" talking "+avrb2+" into their "+adj2+" phones as they eat their "+n2+"");
+		System.out.println("Give me your favorite number: ");
+		String nr1 = in.nextLine();
+		System.out.println("Give me another another another another plural noun: ");
+		String pn4 = in.nextLine();
+		System.out.println("Give me another another noun: ");
+		String n3 = in.nextLine();
+		System.out.println("Give an action you do when you play a sport: ");
+		String vrb2 = in.nextLine();
+		System.out.println("Give me place you stay a longtime in: ");
+		String n4 = in.nextLine();
+		System.out.println(""+nr1+" percent of American "+pn4+" place their "+n3+" calls from their cars as they are "+vrb2+"ing to and from their home, office, or "+n4+". ");
+		System.out.println("Give me a good noun: ");
+		String n5 = in.nextLine();
+		System.out.println("Walking and talking are now the “in” "+n5+" to do. ");
+		System.out.println("Give me a number: ");
+		String n2 = in.nextLine();
+
 		
-		
+
 		in.close();//Don't forget to clean up after yourselves!
 	}
 
